c0dd527657ff3e3f9b8ebfd15cc17a50c8014261
seaskyho <seaskyho@gmail.com>
2024-06-04 11:36:48 +1000

    update

--- scripts/musc.sh
+++ scripts/musc.sh
@@ -18,4 +18,3 @@ python examples/musc_main.py --device 0 \
 --backbone_name ViT-L-14-336 --pretrained openai --feature_layers 5 11 17 23 \
 --img_resize 518 --divide_num 1 --r_list 1 3 5 --batch_size 4 \
 --output_dir ./output --vis True --save_excel True
-
